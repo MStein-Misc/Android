ab3a48f2ee79d5cfef84a577e9844d9676a0c4a1
moshestein <38742596+moshestein@users.noreply.github.com>
2019-02-10 22:35:41 -0500

    init app1

---
+++ app1/app/src/main/java/com/example/moish/helloworld/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.moish.helloworld;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+        Log.d(LOG_TAG, "Hello World");
+    }
+
+    public void showToast(View view) {
+        Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void countUp(View view) {
+//        mCount++;
+        if (mShowCount != null){
+            mShowCount.setText(Integer.toString(++mCount));
+        }
+    }
+}
